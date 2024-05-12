e9312b498b739161ef5451594c48b289a0dc1b25
Edondjiibolo <enzombul@gmail.com>
2024-05-12 13:03:01 +0800

    fix

--- Router/app.js
+++ Router/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import { readFile } from "../service/readFile.js";
 import cors from "cors";
 import { serviceRouter } from "./service-router.js";
-import { ServiceModel } from "../Model/product.js";
 const produts = readFile("../src/mocks/products.json");
 const app = express();
 
--- Router/service-router.js
+++ Router/service-router.js
@@ -2,9 +2,7 @@ import { Router } from "express";
 import { ServiceController } from "../Controller/product.js";
 
 export const serviceRouter = Router();
-serviceRouter.post("/login", ServiceController.loginUser);
-serviceRouter.post("/signin", ServiceController.createUser);
-serviceRouter.post("/account-setting", ServiceController.AccountSetting);
+
 serviceRouter.get("/account-info", ServiceController.AccountInfo);
 serviceRouter.get("/products", ServiceController.getProducts);
 serviceRouter.get("/images/:id", ServiceController.getImage);
